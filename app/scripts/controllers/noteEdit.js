68e6b6ce3dd46cb189ea0b5bae67cc548558f220
Dmitry Olyenyov <dmitry.olyenyov@gmail.com>
2014-01-21 20:15:37 +0400

    Проставляю текущую папку при создании заметки

--- app/scripts/controllers/noteEdit.js
+++ app/scripts/controllers/noteEdit.js
@@ -4,13 +4,12 @@ angular.module('usetechApp')
     .controller('NoteEditCtrl', function($scope, $florm, $routeParams, $window, $location, FolderListService) {
         if ($routeParams.noteId !== undefined) {
             $scope.note = FolderListService.findNote($routeParams.noteId);
+            $scope.noteFolder = $scope.note.folders.id; //Отдельную переменную завёл для того, чтобы обойти зацикливание $digest
         } else {
             $scope.note = FolderListService.createNote();
+            $scope.noteFolder = $routeParams.folderId;
         }
         $scope.folderListService = FolderListService;
-        if ($scope.note.folders !== undefined) {
-            $scope.noteFolder = $scope.note.folders.id; //Отдельную переменную завёл для того, чтобы обойти зацикливание $digest
-        }
 
         $scope.cancel = function() {
             $window.history.back();
